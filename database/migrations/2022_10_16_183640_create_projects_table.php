d4457b2522428f2c6475a7eabce57ae3832398d9
devsocha <konrad.socha5@gmail.com>
2022-10-16 21:09:09 +0200

    Przygotowanie bazdy danych

---
+++ database/migrations/2022_10_16_183640_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id('id_projekt');
+            $table->text('nazwa');
+            $table->text('opis')->nullable();
+            $table->text('kwota_netto');
+            $table->text('rodzaj')->default('nowy');
+            $table->bigInteger('id_osoba');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
