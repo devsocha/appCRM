9e10186293345ce3e10b2fa80061436f7333ab7e
Konrad Socha <konrad.socha5@gmail.com>
2022-10-20 15:51:56 +0200

    Praca nad id usera w wyszukiwaniu

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\company;
 use App\Models\lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LeadController extends Controller
 {
     public function lead(){
-        $firmy = lead::where('id_firma_partner',1)->get();
+        $id_partnera = Auth::user()->id();
+        $firmy = lead::where('id_firma_partner',$id_partnera)->get();
         return view('app_leads',[
             'siteNameTittle' => 'Leady',
             'firmy'=> $firmy,
